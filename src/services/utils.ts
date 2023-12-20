425b0908ac08ad27ac70a9d2c837fd8d491f9034
rxw22 <2587897409@qq.com>
2023-12-20 21:06:00 +0800

    fix: 获取headers

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,21 +1,56 @@
 import { HmacSHA256 } from "crypto-js";
 
-export const picacg = {
-  Url: "https://picaapi.picacomic.com/",
-  ApiKey: "C69BAF41DA5ABD1FFEDC6D2FEA56B",
+const picacg = {
+  api: "https://picaapi.picacomic.com/",
+  apiKey: "C69BAF41DA5ABD1FFEDC6D2FEA56B",
   secretKey: "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn",
 };
 
-export function getSign(
+const defaultHeaders = {
+  accept: "application/vnd.picacomic.com.v1+json",
+  "User-Agent": "okhttp/3.8.1",
+  "Content-Type": "application/json; charset=UTF-8",
+  "api-key": "C69BAF41DA5ABD1FFEDC6D2FEA56B",
+  "app-build-version": "45",
+  "app-channel": "3",
+  "app-platform": "android",
+  "app-uuid": "defaultUuid",
+  "app-version": "2.2.1.3.3.4",
+  "image-quality": "original",
+  nonce: "4ce7a7aa759b40f794d189a88b84aba8",
+  signature: "",
+  time: "",
+  authorization: "",
+};
+
+function getSign(
   url: string,
   timestamp: string,
   nonce: string,
   method: "GET" | "POST"
 ): string {
-  const key =
-    url + timestamp + nonce + method + "C69BAF41DA5ABD1FFEDC6D2FEA56B";
-  return HmacSHA256(
-    key.toLowerCase(),
-    "~d}$Q7$eIni=V)9\\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn"
-  ).toString();
+  const key = url + timestamp + nonce + method + picacg.apiKey;
+  return HmacSHA256(key.toLowerCase(), picacg.secretKey).toString();
 }
+
+const randomString = (e: number = 32) => {
+  let t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+  let a = t.length;
+  let n = "";
+  for (let i = 0; i < e; i++) {
+    n += t.charAt(Math.floor(Math.random() * a));
+  }
+  return n;
+};
+
+export const getHeaders = (
+  url: string,
+  method: "GET" | "POST",
+  options: any = {}
+) => {
+  const headers = { ...defaultHeaders, ...options };
+  headers.time = (new Date().getTime() / 1000).toFixed(0);
+  headers.nonce = randomString(32).toLowerCase();
+  headers.signature = getSign(url, headers.time, headers.nonce, method);
+  return headers;
+};
